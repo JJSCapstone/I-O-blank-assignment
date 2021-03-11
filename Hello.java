435df765d071053a22ada6d0122c85e93900d340
jomoody10 <59669631+jomoody10@users.noreply.github.com>
2021-03-11 16:50:10 -0600

    removed initialization of Scanner and shortened output to "Hello" only

--- Hello.java
+++ Hello.java
@@ -2,11 +2,8 @@ import java.util.Scanner;
 
 public class Hello {
     public static void main(String[] args) {
-        String line;
-        Scanner scanner = new Scanner(System.in);
-
         System.out.println("What is your name?");
-        line = scanner.nextLine();
-        System.out.println("Hello " + line);
+
+        System.out.println("Hello");
     }
 }
